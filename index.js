9392e8b084b1d1195563ec21c1a2238aaf5ef071
Christina Nguyen <cnguyen4290@yahoo.com>
2021-10-28 01:03:45 -0500

    index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 let btn = document.getElementById('btn').addEventListener('click', () => {
-    generateQuote();
+    getQuote();
   });
   
-
-
-const url = "https://parade.com/1213691/alexandra-hurtado/friends-tv-show-quotes/";
-function generateQuote(){
-   fetch(url)
-  .then(function(data) {
-         return data.json();
-    })
-    .then(function(data){    
-    document.getElementById("quote").innerHTML = data.content; 
-    document.querySelector(".character").innerHTML = "- " + data.character;
-   })
-
+  const url = "https://parade.com/1213691/alexandra-hurtado/friends-tv-show-quotes/";
+  function getQuote(){
+     fetch(url)
+    .then(function(data) {
+           return data.json();
+      })
+      .then(function(data){    
+      document.getElementById("quote").innerHTML = data.content; document.querySelector(".character").innerHTML = "- " + data.character;
+     })
+   .catch(function(err) {
+      console.log(err); 
+      });
+   }

39e18f6d8f23e4261b9707b8e814261a7fca75f3
Christina Nguyen <cnguyen4290@yahoo.com>
2021-11-05 09:51:59 -0500

    index.js

--- index.js
+++ index.js
@@ -4,8 +4,29 @@ let btn = document.getElementById('btn').addEventListener('click', () => {
     getQuote();
   });
 
-  fetch('https://friends-quotes-api.herokuapp.com/quotes')
-  .then(response => response.json())
-  .then(data => {
-    console.log(`${data.content} —${data.character}`)
-  })
+  function getQuote() {
+    const xhr = new XMLHttpRequest();
+    const url = 'quotes.json';
+  
+    xhr.open('GET', url, true);
+  
+    xhr.onload = function() {
+      if (this.status === 200) {
+        let quotes = JSON.parse(this.responseText);
+  
+        for (i in quotes) {
+          outputQuote = quotes[i].quote;
+          outputCharacter = quotes[i].character;
+  
+          queryNum = 40;
+          randomNum = Math.floor(Math.random() * queryNum + 1);
+  
+          document.getElementById('output').innerHTML = `
+          "${quotes[randomNum].quote}" - ${quotes[randomNum].character} <br>
+          ${quotes[randomNum].pic}
+          `;
+        }
+      }
+    };
+    xhr.send();
+  }

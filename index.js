ff45ed7139005a0c568b192f2af64a51485bb34f
Christina Nguyen <cnguyen4290@yahoo.com>
2021-11-05 09:47:34 -0500

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var audio = new Audio('https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=I%27ll+be+there+for+you&filename=mt/MTgyODQzMjIxODI5MDI_2PVZSjTrosA.mp3');
-audio.play();
+
 
 let btn = document.getElementById('btn').addEventListener('click', () => {
     getQuote();

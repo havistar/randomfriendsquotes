44de393c29df8713601a92b062188adb7708f05f
Christina Nguyen <cnguyen4290@yahoo.com>
2021-11-08 16:41:50 -0600

    index.js

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ let btn = document.getElementById('btn').addEventListener('click', () => {
     getQuote();
   });
 function getQuote(){
+    if (this.status === 200) {
+        let quotes = JSON.parse(this.responseText);
+  
+        for (i in quotes) {
+          outputQuote = quotes[i].quote;
+          outputCharacter = quotes[i].character;
+  
+          queryNum = 40;
+          randomNum = Math.floor(Math.random() * queryNum + 1);
+          
     let quotes = Math.floor(Math.random()*quotes.length);
 
     document.getElementById('output').innerHTML = `

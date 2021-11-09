316f5fb2c9ab0088428ea6b34a536b50cd281a89
Christina Nguyen <cnguyen4290@yahoo.com>
2021-11-09 17:47:59 -0600

    index.js

--- index.js
+++ index.js
@@ -10,27 +10,12 @@ let btn = document.getElementById('btn').addEventListener('click', () => {
       if (!jsonResponse.length) {
         return []
       }
-      return jsonResponse[Math.floor(Math.random() * jsonResponse.length)]
+      return jsonResponse[Math.floor(Math.random() * jsonResponse.length)];
+
+      document.getElementById('output').innerHTML = `
+      "${quotes[randomNum].quote}" - ${quotes[randomNum].character} <br>
+      ${quotes[randomNum].pic}
+      `;
     })
   }
   
-
-  function getQuote() {
-    if (this.status === 200) {
-      let quotes = JSON.parse(this.responseText);
-
-      for (i in quotes) {
-        outputQuote = quotes[i].quote;
-        outputCharacter = quotes[i].character;
-
-        queryNum = 40;
-        randomNum = Math.floor(Math.random() * queryNum + 1);
-
-        document.getElementById('output').innerHTML = `
-        "${quotes[randomNum].quote}" - ${quotes[randomNum].character} <br>
-        ${quotes[randomNum].pic}
-        `;
-      }
-    }
-  };
-

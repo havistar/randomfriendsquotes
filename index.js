d51da0b156e554d4f19ea51f6f50da11f07e5fde
Christina Nguyen <cnguyen4290@yahoo.com>
2021-11-08 18:51:29 -0600

    index.js

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 
+let btn = document.getElementById('btn').addEventListener('click', () => {
+    getQuote();
+  });
   
- let quote = document.getElementById("quote");
- let character = document.getElementById("character");
- let btn = document.getElementById("btn");
+  function getQuote() {
+    return fetch('quotes.json').then(response => {
+      return response.json()
+    }).then(jsonResponse => {
+      if (!jsonResponse.length) {
+        return []
+      }
+      return jsonResponse[Math.floor(Math.random() * jsonResponse.length)]
+    })
+  }
+  
+  getQuote().then(quote => {
+    const text = quote.text;
+    const character = quote.character;
+    console.log(text, character)
+  })
 
- const url = "quotes.json";
+  function getQuote() {
+    if (this.status === 200) {
+      let quotes = JSON.parse(this.responseText);
 
- let getQuote = () => {
-     fetch(url)
-     .then((data) => data.json())
-     .then((item) => {
-         quote.innerText = item.content;
-         character.innerText = item.character;
-         
-         document.getElementById('output').innerHTML = `
-         "${quotes[randomNum].quote}" - ${quotes[randomNum].character} <br>
-         ${quotes[randomNum].pic}
-         `;
-     });
- };
+      for (i in quotes) {
+        outputQuote = quotes[i].quote;
+        outputCharacter = quotes[i].character;
+
+        queryNum = 40;
+        randomNum = Math.floor(Math.random() * queryNum + 1);
+
+        document.getElementById('output').innerHTML = `
+        "${quotes[randomNum].quote}" - ${quotes[randomNum].character} <br>
+        ${quotes[randomNum].pic}
+        `;
+      }
+    }
+  };
 
  window.addEventListener("load", getQuote);
- btn.addEventListener("click", getQuote);
+ btn.addEventListener("click", getQuote)

230853da4a22960f0d84332f2e4bfee58e19dfb7
Christina Nguyen <cnguyen4290@yahoo.com>
2021-11-09 22:52:55 -0600

    index.js

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
+let quote = document.getElementById("quote");
+let character = document.getElementById("character");
+let btn = document.getElementById("btn");
 
-let btn = document.getElementById('btn').addEventListener('click', () => {
-    getQuote();
-  });
-  
-  function getQuote() {
-    return fetch('quotes.json').then(response => {
-      return response.json()
-    }).then(jsonResponse => {
-      if (!jsonResponse.length) {
-        return []
-      }
-      return jsonResponse[Math.floor(Math.random() * jsonResponse.length)];
+const url = "quotes.json";
 
-      document.getElementById('output').innerHTML = `
-      "${quotes[randomNum].quote}" - ${quotes[randomNum].character} <br>
-      ${quotes[randomNum].pic}
-      `;
-    })
-  }
-  
+let getQuote = () => {
+    fetch(url)
+      .then((data) => data.json())
+      .then((item) => {
+        quote.innerText = item.content;
+        character.innerText = item.character;
+      });
+  };
+
+  window.addEventListener("load", getQuote);
+btn.addEventListener("click", getQuote);

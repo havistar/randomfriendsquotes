e32a02518cd8419c8d84ae7cb2392d5cfb5b64e3
Christina Nguyen <cnguyen4290@yahoo.com>
2022-01-02 20:33:35 -0600

    index.js

--- index.js
+++ index.js
@@ -13,30 +13,31 @@ function myFunction() {
 }
 
   
-function getQuote() {
-  const xhr = new XMLHttpRequest();
-  const url = 'quotes.json';
-
-  xhr.open('GET', url, true);
-
-  xhr.onload = function() {
-    if (this.status === 200) {
-      const data = JSON.parse(jsonRawData);
-    const quotes = data.quotes;
-
-      for (i in quotes) {
-        outputQuote = quotes[i].quote;
-        outputCharacter = quotes[i].character;
-
-        queryNum = 40;
-        randomNum = Math.floor(Math.random() * queryNum + 1);
-
-        document.getElementById('output').innerHTML = `
-        "${quotes[randomNum].quote}" - ${quotes[randomNum].character} <br>
-        ${quotes[randomNum].pic}
-        `;
+  function getQuote() {
+    const xhr = new XMLHttpRequest();
+    const url = 'quotes.json';
+  
+    xhr.open('GET', url, true);
+  
+    xhr.onload = function() {
+      if (this.status === 200) {
+        const jsonRawData = this.responseText;
+const data = JSON.parse(jsonRawData);
+const quotes = data.quotes;
+  
+        for (i in quotes) {
+          outputQuote = quotes[i].quote;
+          outputCharacter = quotes[i].character;
+  
+          queryNum = 40;
+          randomNum = Math.floor(Math.random() * queryNum + 1);
+  
+          document.getElementById('output').innerHTML = `
+          "${quotes[randomNum].quote}" - ${quotes[randomNum].character}<br>
+          ${quotes[randomNum].pic}
+          ` ;
+        }
       }
-    }
-  };
-  xhr.send();
-}
+    };
+    xhr.send();
+  }
